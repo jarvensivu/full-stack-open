0f1b675e040e14450a9cd8a6a3f6e2d6264cc26e
Juha Järvensivu <juha.matti.jarvensivu@gmail.com>
2023-04-17 23:46:13 +0300

    Add exercises 3.1.-3.6

---
+++ part_3/phonebook/index.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const generateID = () => {
+  return Math.floor(Math.random() * 10000);
+};
+
+const isUniqueName = (name) => {
+  return persons.find((person) => person.name === name) === undefined;
+};
+
+app.get("/api/persons", (request, response) => {
+  response.send(persons);
+});
+
+app.post("/api/persons", (request, response) => {
+  if (!request.body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+
+  if (!request.body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
+
+  if (isUniqueName(request.body.name)) {
+    const person = {
+      id: generateID(),
+      name: request.body.name,
+      number: request.body.number,
+    };
+    persons = persons.concat(person);
+    response.json(person);
+  } else {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`
+  );
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
