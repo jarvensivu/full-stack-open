221a222c6e98504c68cbb06f061237334f53ae1c
Juha Järvensivu <juha.matti.jarvensivu@gmail.com>
2023-07-29 23:05:33 +0300

    Add exercise 9.17.

--- part_9/flight-diary/frontend/src/App.tsx
+++ part_9/flight-diary/frontend/src/App.tsx
@@ -1,20 +1,73 @@
 import { useEffect, useState } from "react";
-import { getAllDiaryEntries } from "./services/diaryService";
+import { getAllDiaryEntries, addDiaryEntry } from "./services/diaryService";
 import { DiaryEntry } from "./types";
 
 const App = () => {
   const [diaryEntries, setDiaryEntries] = useState<DiaryEntry[]>([]);
+  const [date, setDate] = useState<string>("");
+  const [visibility, setVisibility] = useState<string>("");
+  const [weather, setWeather] = useState<string>("");
+  const [comment, setComment] = useState<string>("");
 
   useEffect(() => {
     getAllDiaryEntries().then((entries) => {
-      console.log(entries);
       setDiaryEntries(entries);
     });
   }, []);
 
+  const handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    addDiaryEntry({ date, visibility, weather, comment }).then((savedEntry) => {
+      setDiaryEntries(diaryEntries.concat(savedEntry));
+      setDate("");
+      setVisibility("");
+      setWeather("");
+      setComment("");
+    });
+  };
+
   return (
     <div className="App">
       <h1>Flight Diary</h1>
+      <h2>Add new Entry</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="date">Date</label>
+        <input
+          type="date"
+          id="date"
+          name="date"
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+        />
+        <br />
+        <label htmlFor="visibility">Visibility</label>
+        <input
+          type="text"
+          id="visibility"
+          name="visibility"
+          value={visibility}
+          onChange={(event) => setVisibility(event.target.value)}
+        />
+        <br />
+        <label htmlFor="weather">Weather</label>
+        <input
+          type="text"
+          id="weather"
+          name="weather"
+          value={weather}
+          onChange={(event) => setWeather(event.target.value)}
+        />
+        <br />
+        <label htmlFor="comment">Comment</label>
+        <input
+          type="text"
+          id="comment"
+          name="comment"
+          value={comment}
+          onChange={(event) => setComment(event.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
       <h2>Diary entries</h2>
       {diaryEntries.map((entry) => (
         <div key={entry.id}>
--- part_9/flight-diary/frontend/src/services/diaryService.tsx
+++ part_9/flight-diary/frontend/src/services/diaryService.tsx
@@ -1,8 +1,14 @@
 import axios from "axios";
-import { DiaryEntry } from "../types";
+import { DiaryEntry, NewDiaryEntry } from "../types";
 
 const baseUrl = "/api/diaries";
 
 export const getAllDiaryEntries = () => {
   return axios.get<DiaryEntry[]>(baseUrl).then((response) => response.data);
+};
+
+export const addDiaryEntry = (entry: NewDiaryEntry) => {
+  return axios
+    .post<DiaryEntry>(baseUrl, entry)
+    .then((response) => response.data);
 };
