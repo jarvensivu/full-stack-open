84d07fadfcea236b4efe0193c02d55c7a0a078d8
Juha Järvensivu <juha.matti.jarvensivu@gmail.com>
2023-08-05 13:19:43 +0300

    Add exercise 9.25.

--- part_9/patientor/frontend/src/components/PatientPage/index.tsx
+++ part_9/patientor/frontend/src/components/PatientPage/index.tsx
@@ -1,15 +1,121 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Box, CircularProgress, Typography } from "@mui/material";
+import { Box, Stack, CircularProgress, Typography } from "@mui/material";
+import { Work, LocalHospital, HealthAndSafety, Favorite } from '@mui/icons-material';
 import { Male, Female } from '@mui/icons-material';
 import patientService from "../../services/patients";
-import { Diagnosis, Patient } from "../../types";
+import { Diagnosis, Patient, Entry, HealthCheckRating } from "../../types";
+import { assertNever } from "../../utils";
 
-interface Props {
+interface PatientPageProps {
   diagnoses: Diagnosis[];
 }
 
-const PatientPage = ({ diagnoses }: Props) => {
+interface EntryDetailsProps {
+  diagnoses: Diagnosis[];
+  entry: Entry;
+}
+
+interface DiagnosisListProps {
+  diagnoses: Diagnosis[];
+  entry: Entry;
+}
+
+interface HealthRatingBarProps {
+  rating: number;
+}
+
+const HealthRatingBar = ({ rating }: HealthRatingBarProps) => {
+  switch (rating) {
+    case HealthCheckRating.Healthy:
+      return <Favorite style={{color: "#00cc00"}}/>;
+    case HealthCheckRating.LowRisk:
+      return <Favorite style={{color: "#ffff00"}}/>
+    case HealthCheckRating.HighRisk:
+      return <Favorite style={{color: "#ff6600"}}/>
+    case HealthCheckRating.CriticalRisk:
+      return <Favorite style={{color: "#ff0000"}}/>
+    default:
+      return null;
+  }
+}
+
+const DiagnosisList = ({diagnoses, entry}: DiagnosisListProps) => {
+  if (!entry.diagnosisCodes || entry.diagnosisCodes.length === 0) return null;
+
+  return (
+    <ul>
+      {entry.diagnosisCodes?.map(code => (
+        <li key={code}>
+          {code} {diagnoses.find(diagnosis => diagnosis.code === code)?.name}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+const EntryDetails = ({ diagnoses, entry }: EntryDetailsProps ) => {
+  switch (entry.type) {
+    case "Hospital":
+      return (
+      <div>
+        <Typography variant="body1">
+          {entry.date} <LocalHospital />
+        </Typography>
+        <Typography variant="body1">
+          <em>{entry.description}</em>
+        </Typography>
+        <Typography variant="body1">
+          Discharge: {entry.discharge.date} {entry.discharge.criteria}
+        </Typography>
+        <DiagnosisList diagnoses={diagnoses} entry={entry} />
+        <Typography variant="body1">
+          diagnose by {entry.specialist}
+        </Typography>
+      </div>
+      )
+    case "HealthCheck":
+      return (
+        <div>
+          <Typography variant="body1">
+            {entry.date} <HealthAndSafety />
+          </Typography>
+          <Typography variant="body1">
+            <em>{entry.description}</em>
+          </Typography>
+          <HealthRatingBar rating={entry.healthCheckRating} />
+          <DiagnosisList diagnoses={diagnoses} entry={entry} />
+          <Typography variant="body1">
+            diagnose by {entry.specialist}
+          </Typography>
+        </div>
+      );
+    case "OccupationalHealthcare":
+      return (
+        <div>
+          <Typography variant="body1">
+            {entry.date} <Work /> {entry.employerName}
+          </Typography>
+          <Typography variant="body1">
+            <em>{entry.description}</em>
+          </Typography>
+          {entry.sickLeave && (
+            <Typography variant="body1">
+              Sick Leave: {entry.sickLeave.startDate} - {entry.sickLeave.endDate}
+            </Typography>
+          )}
+          <DiagnosisList diagnoses={diagnoses} entry={entry} />
+          <Typography variant="body1">
+            diagnose by {entry.specialist}
+          </Typography>
+        </div>
+      );
+    default:
+      return assertNever(entry);
+  }
+}
+
+const PatientPage = ({ diagnoses }: PatientPageProps) => {
   const [isLoading, setIsLoading] = useState(true);
   const [patient, setPatient] = useState<Patient>();
   const { id } = useParams<{ id: string }>();
@@ -67,26 +173,17 @@ const PatientPage = ({ diagnoses }: Props) => {
           <Typography variant="body1">
             occupation: {patient.occupation}
           </Typography>
-          <Typography variant="h6" sx={{ mt: 3 }}>
+          <Typography variant="h6" sx={{ mt: 3, mb: 1 }}>
             entries
           </Typography>
           {patient.entries.map(entry => (
-            <div key={entry.id}>
-              <Typography variant="body1" sx={{ mt: 1 }}>
-                {entry.date} <em>{entry.description}</em>
-              </Typography>
-              <ul>
-                {entry.diagnosisCodes?.map(code => (
-                  <li key={code}>
-                    {code} {diagnoses.find(diagnosis => diagnosis.code === code)?.name}
-                  </li>
-                ))}
-              </ul>
-            </div>
+            <Stack key={entry.id} sx={{ border: 1, borderRadius: 2, padding: 2, my: 2 }}>
+              <EntryDetails entry={entry} diagnoses={diagnoses} />
+            </Stack>
           ))}
           {patient.entries.length === 0 && (
             <Typography variant="body1" sx={{ mt: 1 }}>
-              No entries
+              no entries
             </Typography>
           )}  
       </Box>
