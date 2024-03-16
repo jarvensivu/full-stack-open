f8355f949c067c5c8bf2d3e6e6c6d04ceee70c88
Juha Järvensivu <juha.matti.jarvensivu@gmail.com>
2024-03-16 18:31:13 +0200

    Setup phonebook endpoint

--- part_2/phonebook/src/services/persons.js
+++ part_2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = import.meta.env.VITE_API_ENDPOINT ?? '/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
