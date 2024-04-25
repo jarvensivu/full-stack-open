5d342cdc74a3d3421a8d80114e33ac9900e0d675
Juha Järvensivu <juha.matti.jarvensivu@gmail.com>
2024-04-25 22:40:51 +0300

    Migrate part 7 to Vite

--- part_7/bloglist/frontend/.eslintrc.js
+++ part_7/bloglist/frontend/.eslintrc.js
@@ -1,56 +1,50 @@
 module.exports = {
-  "env": {
-      "browser": true,
-      "es6": true
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    "vitest-globals/env": true,
+    "cypress/globals": true
   },
-  "extends": [ 
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "prettier"
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:vitest-globals/recommended',
   ],
-  "parserOptions": {
-      "ecmaFeatures": {
-          "jsx": true
-      },
-      "ecmaVersion": 2018,
-      "sourceType": "module"
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react', 'cypress'],
+  rules: {
+    "indent": [
+        "error",
+        2  
+    ],
+    "linebreak-style": [
+        "error",
+        "unix"
+    ],
+    "quotes": [
+        "error",
+        "single"
+    ],
+    "semi": [
+        "error",
+        "never"
+    ],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": [
+        "error", "always"
+    ],
+    "arrow-spacing": [
+        "error", { "before": true, "after": true }
+    ],
+    "no-console": 0,
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": 0,
+    "no-unused-vars": 0    
   },
-  "plugins": [
-      "react", "prettier"
-  ],
-  "rules": {
-      "indent": [
-          "error",
-          2  
-      ],
-      "linebreak-style": [
-          "error",
-          "unix"
-      ],
-      "quotes": [
-          "error",
-          "single"
-      ],
-      "semi": [
-          "error",
-          "never"
-      ],
-      "eqeqeq": "error",
-      "no-trailing-spaces": "error",
-      "object-curly-spacing": [
-          "error", "always"
-      ],
-      "arrow-spacing": [
-          "error", { "before": true, "after": true }
-      ],
-      "no-console": 0,
-      "react/prop-types": 0,
-      "react/react-in-jsx-scope": "off",
-      "prettier/prettier": "error"
-  },
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
-  }
 }
