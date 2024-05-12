163c7b1f123eb41e1b2c333130843318f6aaca95
Juha Järvensivu <juha.matti.jarvensivu@gmail.com>
2024-05-12 21:02:49 +0300

    Migrate library frontend to Vite

--- part_8/library-frontend/src/main.jsx
+++ part_8/library-frontend/src/main.jsx
@@ -27,7 +27,9 @@ const client = new ApolloClient({
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
 );
