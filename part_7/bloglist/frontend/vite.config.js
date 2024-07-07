65207fa1b43cce9e33d0169398a69c40b446d26a
Juha Järvensivu <juha.matti.jarvensivu@gmail.com>
2024-07-07 23:08:20 +0300

    Clean up config file

--- part_7/bloglist/frontend/vite.config.js
+++ part_7/bloglist/frontend/vite.config.js
@@ -13,10 +13,5 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './testSetup.js',
   }
 })
