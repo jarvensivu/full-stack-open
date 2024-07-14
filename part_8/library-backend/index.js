ee4bacca395e3a2bb484740798c71976f886928a
Juha Järvensivu <juha.matti.jarvensivu@gmail.com>
2024-07-14 20:05:08 +0300

    Extract schema definitions to new file

--- part_8/library-backend/index.js
+++ part_8/library-backend/index.js
@@ -9,6 +9,8 @@ const Book = require("./models/book");
 const Author = require("./models/author");
 const User = require("./models/user");
 
+const typeDefs = require("./schema")
+
 require("dotenv").config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -23,63 +25,6 @@ mongoose
   .catch((error) => {
     console.log("error connection to MongoDB:", error.message);
   });
-
-const typeDefs = `
-  type Author {
-    name: String!
-    born: Int
-    bookCount: Int!
-    id: ID!
-  }
-
-  type Book {
-    title: String!
-    published: Int!
-    author: Author!
-    id: ID!
-    genres: [String!]!
-  }
-
-  type User {
-    username: String!
-    favoriteGenre: String!
-    id: ID!
-  }
-
-  type Token {
-    value: String!
-  }
-
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-    allBooks(author: String, genre: String): [Book]!
-    allGenres: [String]!
-    allAuthors: [Author]!
-    me: User
-  }
-
-  type Mutation {
-    addBook(
-      title: String!
-      published: Int!
-      author: String!
-      genres: [String]!
-    ): Book
-    editAuthor(
-      name: String!,
-      setBornTo: Int!
-    ): Author
-    createUser(
-      username: String!
-      favoriteGenre: String!
-    ): User
-    login(
-      username: String!
-      password: String!
-    ): Token
-  }
-`;
 
 const resolvers = {
   Query: {
