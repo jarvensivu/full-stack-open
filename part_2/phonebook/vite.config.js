dc729e1bf971ecb5a804c71ad5b7bd1252b47f8c
Juha Järvensivu <juha.matti.jarvensivu@gmail.com>
2026-01-11 16:25:27 +0200

    Update vite config

--- part_2/phonebook/vite.config.js
+++ part_2/phonebook/vite.config.js
@@ -5,13 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: '127.0.0.1',
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        changeOrigin: true
       },
     },
   },
